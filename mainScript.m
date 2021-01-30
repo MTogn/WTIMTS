e188adaaf2cb7ead23929e457046a3559d2ed23d
MT <Michael.Togneri@gmail.com>
2021-01-30 23:39:54 +0000

    Fixed excess output to screen, reordered preprocessing

--- mainScript.m
+++ mainScript.m
@@ -1,12 +1,15 @@
-burstStartIndex = 5;
-burstEndIndex = 1470;
+%Anything that needs to happen before the main loop over our shorter
+%averaging periods goes here.
+tiltDataLong = WADZPreprocessing();
+
+burstStartIndex = 10;
+burstEndIndex = 20;
 
 %Burst loop
 for burstCtr = burstStartIndex:burstEndIndex
-    
-    tiltDataLong = WADZPreprocessing();
+    tic
     %Import a burst into the workspace
-    [burstDatenums,burstBeamVelocities] = importWADZBurst(burstCtr,tiltDataLong)
+    [burstDatenums,burstBeamVelocities] = importWADZBurst(burstCtr,tiltDataLong);
 
     %Calculate TKE for burst
     
@@ -16,6 +19,7 @@ for burstCtr = burstStartIndex:burstEndIndex
 
     %Calc burst turbulence
     
+    toc
 %End burst loop
 end
 
